74043a1802034174a94df4e23b17612359911b85
Andrés Delgad <andresdelgadoc98@gmail.com>
2023-10-02 13:47:26 -0600

    table

--- src/components/views/users.js
+++ src/components/views/users.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material/';
+import { styled  } from '@mui/system';
 
-export default function users() {
-  return (
-    <div>users</div>
-  )
-}
+// Definimos componentes styled para los estilos
+const StyledHeaderCell = styled(TableCell)(({ theme }) => ({
+    backgroundColor: '#0000FF', // Azul fuerte
+    color: 'white',
+  }));
+  
+  const StyledCell = styled(TableCell)(({ theme }) => ({
+    backgroundColor: '#ADD8E6', // Azul claro
+  }));
+  function CustomTable() {
+    // Datos de ejemplo
+    const rows = [
+      { name: "Juan", age: 28 },
+      { name: "María", age: 24 },
+      { name: "Carlos", age: 30 },
+    ];
+  
+    return (
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <StyledHeaderCell>Nombre</StyledHeaderCell>
+              <StyledHeaderCell>Edad</StyledHeaderCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <StyledCell>{row.name}</StyledCell>
+                <StyledCell>{row.age}</StyledCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+  
+  export default CustomTable;
